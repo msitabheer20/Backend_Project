90f93248630c28e1c3c7502be69637f49f0ef9cb
msitabheer20 <msitabheerdey@gmail.com>
2023-12-04 14:35:31 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose'
-// import {DB_NAME} from "../constants.js";
-const DB_NAME = 'backendProject'
+import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
 
